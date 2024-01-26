6e1fd9bff2f192163fdac2524bf9ce6917d4e30f
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-01-27 01:16:46 +0530

    success implementing infinite scrolling

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -28,8 +28,6 @@ export default class News extends Component {
     };
   }
 
-  
-
   componentDidMount() {
     this.update()
   }
@@ -43,12 +41,33 @@ export default class News extends Component {
     }
   }
 
-  async update(){
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 }, async () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=c3f9a26c882d473db649b4307afdca35&pagesize=${this.state.resultPerPage}&page=${this.state.page}`;
+  
+      let data = await (await fetch(url)).json();
+  
+      // Filter out duplicate articles
+      let newArticles = data.articles.filter((article) =>
+        !this.state.articles.find((a) => a.title === article.title)
+      );
+  
+      this.setState({ articles: this.state.articles.concat(newArticles), totalResults: data.totalResults });
+    });
+  }
+  
+  async update() {
     this.setState({ loading: true });
     try {
       let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=c3f9a26c882d473db649b4307afdca35&pagesize=${this.state.resultPerPage}&page=${this.state.page}`;
       let data = await (await fetch(url)).json();
-      this.setState({ articles: data.articles, totalResults: data.totalResults});
+  
+      // Filter out duplicate articles
+      let newArticles = data.articles.filter((article) =>
+        !this.state.articles.find((a) => a.title === article.title)
+      );
+  
+      this.setState({ articles: this.state.articles.concat(newArticles), totalResults: data.totalResults });
     } catch (error) {
       console.error(error);
     } finally {
@@ -56,82 +75,45 @@ export default class News extends Component {
     }
   }
 
-  handleNext = async () => {
-    this.state.page++;
-    this.update();
-  };
-
-  handlePrev = async () => {
-    this.state.page--;
-    this.update();
-  };
-
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=c3f9a26c882d473db649b4307afdca35&pagesize=${this.state.resultPerPage}&page=${this.state.page}`;
-    let data = await (await fetch(url)).json();
-    this.setState({ articles: this.state.articles.concat(data.articles), totalResults: data.totalResults});
-  }
-
   render() {
     return (
-      <div className="container my-3">
+      <div className="my-3" >
         <h2
           className="news-heading text-center tw-text-4xl"
           style={{ margin: "35px 0px" }}
         >
           NewHUB - Top {this.props.category} headlines - page {this.state.page}
         </h2>
-        {/* {this.state.loading && <Spinner />} */}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
           hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner/>}
+          loader={<Spinner />}
+          style={{overflow: 'hidden', alignItems: 'center', padding: '0cm 0cm 1cm 0cm'}}
         >
-        <div className="row my-2">
-          {this.state.articles.map((e) => {
-              return (
-                <div className="col-md-3" key={e.url}>
-                  <NewsItem 
-                    title={ e.title ? e.title.length > 45 ? e.title.slice(0, 45) + "..." : e.title.slice(0, 45) : "" }
-                    desc={ e.description ? e.description.length > 85 ? e.description.slice(0, 85) + "..." : e.description.slice(0, 85) : "" }
-                    newsurl={e.url}
-                    imurl={
-                      e.urlToImage
-                        ? e.urlToImage
-                        : "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o="
-                    }
-                    author={e.author}
-                    date={e.publishedAt}
-                  />
-                </div>
-              );
-            })}
-        </div>
-
+          <div className="container">
+            <div className="row" >
+              {this.state.articles.map((e) => {
+                return (
+                  <div className="col-md-3" key={e.url} >
+                    <NewsItem
+                      title={e.title ? e.title.length > 45 ? e.title.slice(0, 45) + "..." : e.title.slice(0, 45) : ""}
+                      desc={e.description ? e.description.length > 85 ? e.description.slice(0, 85) + "..." : e.description.slice(0, 85) : ""}
+                      newsurl={e.url}
+                      imurl={
+                        e.urlToImage
+                          ? e.urlToImage
+                          : "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o="
+                      }
+                      author={e.author}
+                      date={e.publishedAt}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-primary tw-bg-blue-500 tw-text-white tw-py-2 tw-px-4 tw-border-b-4 tw-border-blue-700 tw-border-solid tw-rounded hover:tw-bg-blue-700 tw-transition tw-duration-500 tw-ease-in-out"
-            onClick={this.handlePrev}
-          >
-            Previous page
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.state.resultPerPage)
-            }
-            type="button"
-            className="btn btn-primary tw-bg-blue-500 tw-text-white tw-py-2 tw-px-4 tw-border-b-4 tw-border-blue-700 tw-border-solid tw-rounded hover:tw-bg-blue-700 tw-transition tw-duration-500 tw-ease-in-out"
-            onClick={this.handleNext}
-          >
-            Next page
-          </button>
-        </div> */}
       </div>
     );
   }
