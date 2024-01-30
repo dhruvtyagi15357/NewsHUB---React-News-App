6fd793eb9d9cefa35ea2ec5b511b27b04378e07f
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-01-30 16:24:18 +0530

    refactored all components to function based components .

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -7,131 +7,150 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { useEffect } from "react";
 
 const News = (props) => {
-
-  const [articles, setArticles] = React.useState([])
-  const [loading, setLoading] = React.useState(false)
-  const [page, setPage] = React.useState(1)
-  const [resultPerPage, setResultPerPage] = React.useState(props.pageSize)
-  const [totalResults, setTotalResults] = React.useState(0)
-  const [progress, setProgress] = React.useState(0)
+  const [articles, setArticles] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [page, setPage] = React.useState(1);
+  const [resultPerPage, setResultPerPage] = React.useState(props.pageSize);
+  const [totalResults, setTotalResults] = React.useState(0);
+  const [progress, setProgress] = React.useState(0);
 
   // in order to use componentDidMount() in functional component,
   // we use useEffect() hook
   useEffect(() => {
     document.title = `${props.category} - NewHUB`;
-    update()
-  }, [])
-  
+    update();
+  }, []);
+
   useEffect(() => {
     document.title = `${props.category} - NewHUB`;
-    categoryUpdate()
-  }, [props.category, props.country])
-  
+    categoryUpdate();
+  }, [props.category, props.country]);
 
   const categoryUpdate = async () => {
     setLoading(true);
     try {
       let url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.api_key}&pagesize=${resultPerPage}&page=${page}`;
       let data = await (await fetch(url)).json();
-  
-      // Filter out duplicate articles
-      let newArticles = data.articles.filter((article) =>
-        !articles.find((a) => a.title === article.title)
-      );
-  
-      setArticles(newArticles)
-      setTotalResults(data.totalResults)
-      setPage(1)
 
+      // Filter out duplicate articles
+      let newArticles = data.articles.filter(
+        (article) => !articles.find((a) => a.title === article.title)
+      );
+
+      setArticles(newArticles);
+      setTotalResults(data.totalResults);
+      setPage(1);
     } catch (error) {
       console.error(error);
     } finally {
-      setLoading(false );
+      setLoading(false);
     }
-  }
+  };
 
   const update = async () => {
     setLoading(true);
-    try { 
+    try {
       let url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.api_key}&pagesize=${resultPerPage}&page=${page}`;
       let data = await (await fetch(url)).json();
-  
+
       // Filter out duplicate articles
-      let newArticles = data.articles.filter((article) =>
-        !articles.find((a) => a.title === article.title)
+      let newArticles = data.articles.filter(
+        (article) => !articles.find((a) => a.title === article.title)
       );
-  
-      setArticles(articles.concat(newArticles))
-      setTotalResults(data.totalResults)
+
+      setArticles(articles.concat(newArticles));
+      setTotalResults(data.totalResults);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (page > 1) {
+      // to avoid running on initial render
+      fetchMoreData();
+    }
+  }, [page]); // this effect runs whenever `page` changes
 
   const fetchMoreData = async () => {
     setLoading(true);
-    setPage(page + 1)
-    try { 
+    try {
       let url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.api_key}&pagesize=${resultPerPage}&page=${page}`;
       let data = await (await fetch(url)).json();
-  
+
       // Filter out duplicate articles
-      let newArticles = data.articles.filter((article) =>
-        !articles.find((a) => a.title === article.title)
+      let newArticles = data.articles.filter(
+        (article) => !articles.find((a) => a.title === article.title)
       );
-  
-      setArticles(articles.concat(newArticles))
-      setTotalResults(data.totalResults)
+
+      setArticles(articles.concat(newArticles));
+      setTotalResults(data.totalResults);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
-    return (
-      <div className="my-3" >
-        <h2
-          className="news-heading text-center tw-text-4xl"
-          style={{ margin: "35px 0px" }}
-        >
-          NewHUB - Top {props.category} headlines - page {page}
-        </h2>
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-          style={{overflow: 'hidden', alignItems: 'center', padding: '0cm 0cm 1cm 0cm'}}
-        >
-          <div className="container">
-            <div className="row" >
-              {articles.map((e) => {
-                return (
-                  <div className="col-md-3" key={e.url} >
-                    <NewsItem
-                      title={e.title ? e.title.length > 45 ? e.title.slice(0, 45) + "..." : e.title.slice(0, 45) : ""}
-                      desc={e.description ? e.description.length > 85 ? e.description.slice(0, 85) + "..." : e.description.slice(0, 85) : ""}
-                      newsurl={e.url}
-                      imurl={
-                        e.urlToImage
-                          ? e.urlToImage
-                          : "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o="
-                      }
-                      author={e.author}
-                      date={e.publishedAt}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+  return (
+    <div className="my-3">
+      <h2
+        className="news-heading text-center tw-text-4xl"
+        style={{ margin: "35px 0px" }}
+      >
+        NewHUB - Top {props.category} headlines - page {page}
+      </h2>
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={() => setPage(page + 1)}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+        style={{
+          overflow: "hidden",
+          alignItems: "center",
+          padding: "0cm 0cm 1cm 0cm",
+        }}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((e) => {
+              return (
+                <div className="col-md-3" key={e.url}>
+                  <NewsItem
+                    title={
+                      e.title
+                        ? e.title.length > 45
+                          ? e.title.slice(0, 45) + "..."
+                          : e.title.slice(0, 45)
+                        : ""
+                    }
+                    desc={
+                      e.description
+                        ? e.description.length > 85
+                          ? e.description.slice(0, 85) + "..."
+                          : e.description.slice(0, 85)
+                        : ""
+                    }
+                    newsurl={e.url}
+                    imurl={
+                      e.urlToImage
+                        ? e.urlToImage
+                        : "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=2048x2048&w=is&k=20&c=ZyHCcX0F_DVM-r_R_vG8OX_CqYLb-G16afTyaVGtB3o="
+                    }
+                    author={e.author}
+                    date={e.publishedAt}
+                  />
+                </div>
+              );
+            })}
           </div>
-        </InfiniteScroll>
-      </div>
-    );
-  }
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
 
 export default News;
 
